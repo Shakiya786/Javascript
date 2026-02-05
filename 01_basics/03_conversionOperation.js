473b08769a68c91483e2df405e1d8603308a546d
Shakiya Bano <shakiyabano911@gmail.com>
2026-02-05 15:30:02 +0000

    conversion and operrations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -11,7 +11,7 @@ let valueInNumber = Number(score)
 
 let isLoggedIn="soni"
 let booleanIsLoggedIn= Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -19,5 +19,37 @@ console.log(booleanIsLoggedIn);
  
 let aNumber = 33
 let stringNumber = String(aNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// *********************** Operations ****************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+//console.log(2+2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2%3);
+
+let str1 = "hello"
+let str2 = "soni"
+
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(2+2+"1");
+
+console.log(+true);
+console.log(+"");
+
+
+
+
+
+
+
