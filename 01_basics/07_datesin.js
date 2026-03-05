dc0d1237134990ba4e752892fbc8b15a8fda0038
Shakiya Bano <shakiyabano911@gmail.com>
2026-03-05 19:30:53 +0000

    07_datesin.js

---
+++ 01_basics/07_datesin.js
@@ -0,0 +1,18 @@
+// Dates
+
+let myDate = new Date()
+//console.log(myDate.toDateString());
+//console.log(myDate.toLocaleDateString());
+//console.log(typeof myDate);
+//console.log(myDate.toTimeString());
+//console.log(myDate.toUTCString());
+
+//let myCreatedDate = new Date(2023,0,23)
+//console.log(myCreatedDate.toDateString());
+//let myCreatedDate = new Date(2023,0,23,5,3)
+//console.log(myCreatedDate.toLocaleString());
+//let myCreatedDate = new Date("2023-01-14")
+//console.log(myCreatedDate.toLocaleString());
+let myCreatedDate = new Date("01-14-2024")
+console.log(myCreatedDate.toLocaleString());
+
