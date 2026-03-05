f79f5835bc30b231e22c7d494291383ffc0aaa51
Shakiya Bano <shakiyabano911@gmail.com>
2026-03-05 18:59:29 +0000

    06_nums_and_maths

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,29 +1,29 @@
 const score = 400
-//console.log(score);
+console.log(score);
 
 const balance = new Number(100)
-//console.log(balance);
+console.log(balance);
 
-//console.log(balance.toString().length);
-//console.log(balance.toFixed(2));
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
 
 const otherNumber = 23.567
 
-//console.log(otherNumber.toPrecision(3));
+console.log(otherNumber.toPrecision(3));
 
 const hundreds = 100000000
-//console.log(hundreds.toLocaleString());//US value
-//console.log(hundreds.toLocaleString('en-IN'));// Indian value
+console.log(hundreds.toLocaleString());//US value
+console.log(hundreds.toLocaleString('en-IN'));// Indian value
 
 // +++++++++++++++++++ Maths ++++++++++++++++++
 
 console.log(Math);
-//console.log(Math.abs(-4));
-//console.log(Math.round(4.6));
-//console.log(Math.ceil(4.2));
-//console.log(Math.floor(4.9));
-//console.log(Math.min(4,2,5,1));
-//console.log(Math.max(3,5,7,8));
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(4,2,5,1));
+console.log(Math.max(3,5,7,8));
 
 console.log(Math.random());// it will give values in between 0 and 1
 console.log((Math.random()*10)+1); // to avoiding zero case we add 1 
