dcb5052b51730f7fc4573a5408feb66308145a7a
Shakiya Bano <shakiyabano911@gmail.com>
2026-03-06 06:45:50 +0000

    07_datesin.js

--- 01_basics/07_datesin.js
+++ 01_basics/07_datesin.js
@@ -7,12 +7,34 @@ let myDate = new Date()
 //console.log(myDate.toTimeString());
 //console.log(myDate.toUTCString());
 
-//let myCreatedDate = new Date(2023,0,23)
+let myCreatedDate = new Date(2023,0,23)
 //console.log(myCreatedDate.toDateString());
 //let myCreatedDate = new Date(2023,0,23,5,3)
 //console.log(myCreatedDate.toLocaleString());
 //let myCreatedDate = new Date("2023-01-14")
-//console.log(myCreatedDate.toLocaleString());
-let myCreatedDate = new Date("01-14-2024")
-console.log(myCreatedDate.toLocaleString());
 
+//console.log(myCreatedDate.toLocaleString());
+//let myCreatedDate = new Date("01-14-2024")
+//console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+//console.log(myTimeStamp);
+//console.log(myCreatedDate.getTime());
+
+console.log(Date.now());
+
+//console.log(myTimeStamp);
+//console.log(myCreatedDate.getTime());
+//console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getMonth() +1);
+console.log(newDate.getDay());
+
+//` ${newDate.getDay()} and the time `
+
+newDate.toLocaleString('default',{
+    weekday:"long",
+})
