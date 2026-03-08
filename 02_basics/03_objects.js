8dce08ab366defb597589e7c9fea9942994dc2ba
Shakiya Bano <shakiyabano911@gmail.com>
2026-03-08 11:56:07 +0000

    03_objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,15 @@
+//singleton
+//object.create
+
+//object literals
+const jsUser = {
+      name:" Salman",
+      age: 20,
+      location: "kolkata",
+      email:"abc@.com",
+      isLoggedIn: false,
+      lastLoginDays: ["Monday","Saturday"]
+}
+
+console.log(jsUser.email)
+console.log(jsUser["email"])
