001f6c41efd8ba589bfcf5e2bbd47ddbddd29f43
Shakiya Bano <shakiyabano911@gmail.com>
2026-03-09 11:32:45 +0000

    03_objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,8 +2,12 @@
 //object.create
 
 //object literals
+
+const mySym = Symbol("key1")
 const jsUser = {
       name:" Salman",
+      "full name" : "Salman khan",
+     [mySym]:"mykey1",
       age: 20,
       location: "kolkata",
       email:"abc@.com",
@@ -11,5 +15,23 @@ const jsUser = {
       lastLoginDays: ["Monday","Saturday"]
 }
 
-console.log(jsUser.email)
-console.log(jsUser["email"])
+//console.log(jsUser.email)
+//console.log(jsUser["email"])
+//console.log(jsUser["full name"])
+//console.log( jsUser[mySym])
+
+jsUser.email = "dab@mail.com"
+//Object.freeze(jsUser)
+jsUser.email = "oksn@mail.com"
+//console.log(jsUser)
+
+jsUser.greeting = function(){
+      console.log("Hello js User");
+}
+
+console.log(jsUser.greeting);
+
+jsUser.greeting2 = function(){
+      console.log(`Hello js User,${this.name}`);
+}
+console.log(jsUser.greeting2());
