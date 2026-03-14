ed7ed7f7d70f2cb6ece2aeaab1907d602c69d5ef
Shakiya Bano <shakiyabano911@gmail.com>
2026-03-14 20:35:28 +0000

    01_functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -15,4 +15,33 @@ function addTwoNumbers(number1,number2){
 }
 const result = addTwoNumbers(5,6)
 
-console.log("result:",result);
+//console.log("result:",result);
+
+function loginUserMsg(username = "Sam"){
+    if(username===undefined){ // equivalent to if(!username)
+           console.log("please enter a username");
+           return
+    }
+    return`${username} just logged in`
+}
+
+//console.log (loginUserMsg("hero"))
+//console.log(loginUserMsg())
+
+function calculateCarPrice(val1, val2, ...num1){ // ... it is rest operator or spread operator depending on its uses
+    return num1
+}
+
+//console.log(calculateCarPrice(200 ,400 , 650 ,1000))
+
+const user = {
+     username:"hero",
+     price:200
+}
+
+function handleObject(object){
+    console.log(`Username is ${object.username} and price is ${object.price}`);
+}
+handleObject(user)
+
+
